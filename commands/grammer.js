92bcc833bed88fdd6d1ec70df25989024e600049
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2021-05-06 02:15:06 +0300

    Update grammer.js

--- commands/grammer.js
+++ commands/grammer.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: 'grammer',
     description: "this is a grammer command",
-    execute(message, Discord){
+    execute(message, args, Discord){
     const grammerembed = new Discord.MessageEmbed()
         .setColor('#FFA500')
-        .setTitle('check your grammer')
+        .setTitle(MessageMentions,'check your grammer')
         .setDescription('you have tested the timeing of this ferret')
         .setImage('https://cdn.onlinewebfonts.com/svg/img_531810.png');
 
