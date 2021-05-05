4f4b7d6dc1d6d11ad7deef822c984121989d9704
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2021-05-05 22:49:55 +0300

    Update nick.js
    You pingus you forgot to add the figurative brackets

--- commands/nick.js
+++ commands/nick.js
@@ -1,9 +1,10 @@
 module.exports = {
     name: 'nick',
     description: "this is a command thats changes your nickname!",
-    execute(message, args, Discord)
+    execute(message, args, Discord){
    
     client.setNickname({nick: message.content.replace('nick ', '')})
     
         message.channel.send('nickname has been changed!');
-}
+    }
+};
