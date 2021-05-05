c5026f03bddc5a9bd280159c13c362fb5afd9e73
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2021-05-06 01:58:42 +0300

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -2,9 +2,24 @@ module.exports = {
     name: 'nick',
     description: "this is a command thats changes your nickname!",
     execute(message, args, Discord){
-   
-    client.setNickname({nick: message.content.replace('nick ', '')})
+        if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`)
+        }
+        if (!message.mentions.users.first()){
+            target = message.author
+        }
+        else{
+            target = message.mentions.users.first()
+            args.shift()
+        }
+        
+        const member = message.guild.members.cache.get(target.id)
+
+        const nickname = args.join(' ')
+
+
+        member.setNickname(nickname)
     
-        message.channel.send('nickname has been changed!');
+    message.reply(nickname);
     }
 };
