8d8c940a9e0ff2f6f3f8d6e7ea3c622863210f62
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2021-05-05 03:01:46 +0300

    Add files via upload

---
+++ commands/grammer.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'grammer',
+    description: "this is a grammer command",
+    execute(message, args, Discord){
+    const grammerembed = new Discord.MessageEmbed()
+        .setColor('#FFA500')
+        .setTitle(MessageMentions, 'check your grammer')
+        .setDescription('you have tested the timeing of this ferret')
+        .setImage('https://cdn.onlinewebfonts.com/svg/img_531810.png');
+
+        
+        message.channel.send(grammerembed)
+    }
+
+};
---
+++ commands/help.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'help',
+    description: "this is a help command",
+    execute(message, args, Discord){
+    const helpembed = new Discord.MessageEmbed()
+        .setColor('#FFA500')
+        .setTitle('COMMANDS')
+        .setDescription('this is a list of command thst are useable')
+        .addFields(
+        {name: 'ping', value: 'this will test the timing of the ferret and if its active of not.' },
+        {name: 'grammer', value: 'make someone check thier grammer'},
+        {name: 'command3', value: 'this is a placeholder'}
+        )
+
+        message.channel.send(helpembed)
+
+    }
+};
