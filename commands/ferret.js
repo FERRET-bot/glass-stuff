2d1cf78e7563c3d58be0ec2c74776e3bde9179cc
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2021-05-06 04:47:28 +0300

    Create ferret.js

---
+++ commands/ferret.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'ferret',
+    description: "this is a command for ferret",
+    execute(message, Discord){
+        message.channel.send('Go the fuck to sleep!')
+        message.channel.send('https://img.freepik.com/free-photo/cute-ferret-is-sleeping_369656-37.jpg?size=626&ext=jpg')
+    }
+};
