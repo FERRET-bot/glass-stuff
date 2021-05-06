f0c25320836a48e03ea61bb42694afc5ed95ea7e
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2021-05-06 17:24:35 +0300

    Update ferret.js

--- commands/ferret.js
+++ commands/ferret.js
@@ -2,7 +2,11 @@ module.exports = {
     name: 'ferret',
     description: "this is a command for ferret",
     execute(message, Discord){
-        message.channel.send('Go the fuck to sleep!')
-        message.channel.send('https://img.freepik.com/free-photo/cute-ferret-is-sleeping_369656-37.jpg?size=626&ext=jpg')
-    }
+        const ferretembed = new Discord.MessageEmbed()
+            .setColor('#FFA500')
+            .setTitle( 'Go the fuck to sleep!')
+            .setImage('https://img.freepik.com/free-photo/cute-ferret-is-sleeping_369656-37.jpg?size=626&ext=jpg');
+            
+            message.channel.send(ferretembed)
+        }
 };
