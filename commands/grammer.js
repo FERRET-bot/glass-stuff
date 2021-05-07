7ea95460bbe26c545c80189d0e2aabe0bb183ba6
Turtle-king <81062054+Turtle-king@users.noreply.github.com>
2021-05-07 20:26:38 +0100

    Update grammer.js

--- commands/grammer.js
+++ commands/grammer.js
@@ -4,8 +4,9 @@ module.exports = {
     execute(message, Discord){
     const grammerembed = new Discord.MessageEmbed()
         .setColor('#FFA500')
-        .setTitle('check your grammer')
-        .setImage('https://cdn.onlinewebfonts.com/svg/img_531810.png');
+        .setTitle('check your grammer you little carrot')
+        .setDescription('you better change you grammer before i spank you')
+        .setImage('https://cdn.discordapp.com/attachments/838500668696166471/838704894340628530/ferret_god.jpg');
 
         
         message.channel.send(grammerembed)
