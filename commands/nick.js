61d1b5cd57bd0f2ff2f6c57ff985a5e98fecb17b
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2021-05-08 18:42:28 +0300

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -1,28 +1,40 @@
+const { DiscordAPIError } = require("discord.js");
+
 module.exports = {
     name: 'nick',
     description: "this is a command thats changes your nickname!",
     execute(message, args, Discord){
-        if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`)
-        }
+        
         if (!message.mentions.users.first()){
             target = message.author
         }
         else{
-            target = message.mentions.users.first()
-            args.shift()
+            if (message.member.hasPermission("MANAGE_NICKNAMES")){
+                target = message.mentions.users.first()
+                args.shift()
+                }
+                else{
+                 message.channel.send("You lack persmissions to use this command")
+                 return 0
+                }
+           
         }
-        
+        if (!args.length) {
+			message.channel.send(`You didn't provide any arguments, ${message.author}!`)
+            return 0
+        }
+      
         const member = message.guild.members.cache.get(target.id)
 
         const nickname = args.join(' ')
 
-
-        member.setNickname(nickname).then(() => {
+       
+           member.setNickname(nickname).then(() => {
             message.reply('nickname changed');
         })
         .catch(err => {
             message.reply('i was unable to change the nickname of that user');
         })
 
-}};
+    }
+};
