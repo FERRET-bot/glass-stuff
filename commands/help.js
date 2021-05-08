3522df7165a0a1b3792a78dcb55628af4c0fa8f7
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2021-05-08 20:07:31 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,15 +5,14 @@ module.exports = {
     const helpembed = new Discord.MessageEmbed()
         .setColor('#FFA500')
         .setTitle('COMMANDS')
-        .setDescription('this is a list of command thst are useable')
+        .setDescription('this is a list of command that are useable')
         .addFields(
-        {name: 'ping', value: 'this will test the timing of the ferret and if its active of not.' },
-        {name: 'grammer', value: 'make someone check thier grammer'},
-        {name: 'nick', value: 'change your nickname faster'},
-        {name: 'commandPH', value: 'this is a place holder'},
-        {name: 'commandPH', value: 'this is a place holder'},
+            {name: 'ping', value: 'this will test the timing of the ferret and if its active of not.' },
+            {name: 'grammer', value: 'make someone check thier grammer'},
+            {name: 'nick', value: 'change your nickname faster'},
+            {name: 'sleep', value: 'tell someone or yourself to go to sleep'},
+            {name: 'ferret', value: 'shows random (and extremely cute) ferret'},
         )
-
         message.channel.send(helpembed)
 
     }
