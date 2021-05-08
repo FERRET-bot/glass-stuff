6e7786ee3f40d38ffdbe176e248f515d849ca12e
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2021-05-08 20:10:38 +0300

    Create sleep.js

---
+++ commands/sleep.js
@@ -0,0 +1,20 @@
+const { MessageAttachment } = require('discord.js');
+module.exports = {
+    
+    name: 'sleep',
+    description: "this is a command for sleep",
+    execute(message, Discord){
+        if (!message.mentions.users.first()){
+            target = message.author
+        }
+        else{
+            target = message.mentions.users.first()
+        }
+    
+        const attachment = new MessageAttachment('https://img.freepik.com/free-photo/cute-ferret-is-sleeping_369656-37.jpg?size=626&ext=jpg');
+       
+        message.delete()
+    message.channel.send(`${target}, go the fuck to sleep!`, attachment)
+    
+    }
+};
