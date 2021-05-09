af695956172a6048d0b4e7ba0c93d370b94f565d
notthatkuna <new9gm@gmail.com>
2021-05-09 16:25:14 -0400

    Add updates to ready event

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,18 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
+        var os = require('os');
+        var sysinfo = {};
+
+        sysinfo.hostname = os.hostname();
+        sysinfo.os = {};
+        sysinfo.os.release          = os.release();
+        sysinfo.os.platform         = os.platform();
+        sysinfo.os.arch             = os.arch();
 		const dt = new Date();
         const msg = new Discord.MessageEmbed()
         .setTitle("Bot Started")
-        .setDescription("Started on "+dt.toString())
+        .setDescription("Started on "+dt.toString()+"\n``PRIVATE_INFO``|``"+sysinfo.os.release.toString()+"``|``"+sysinfo.os.platform.toString()+"``|``"+sysinfo.os.arch.toString()+"``")
         var channels = client.channels
         var cache = channels.cache
         var getter = cache.get("841008670125064223")
