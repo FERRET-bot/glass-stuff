0ea0403df73772f084d447c2fd14c1ab27a3865b
notthatkuna <new9gm@gmail.com>
2021-05-10 14:46:23 -0400

    Add devnames to config, fix info.js errors, fix info.js formatting

--- commands/Information/info.js
+++ commands/Information/info.js
@@ -1,25 +1,22 @@
 const Discord = require('discord.js');
 var fs = require('fs');
 const { version } = require('os');
-const { config } = require('process');
 module.exports = {
     name: "info",
     desc: "Shows info about the bot.",
-    usage: "info",
+    usage: "",
     cooldown: 5,
-    execute: async (message, args, bot) => {
-      let prefix = "!"
-      message.channel.send("My prefix is \""+prefix+"\"")
+    execute: async (message, args, bot, config) => {
+      message.channel.send("My prefix is \""+config.prefix+"\"")
 
       let info = new Discord.MessageEmbed
       info.setTitle('the current info about the bot')
       info.setThumbnail("https://www.bing.com/images/blob?bcid=RBE-Tm.CxroC5NvTZVNmjsTFKyOl.....88")
       info.setColor('#28ff00')
       info.setDescription('the info seen below is the newest info about the bot')
-      info.addField('owners', 'DISCORD.BOT, jaelyn')
-      info.addField('prefix', '!')
-      info.addField('verson', '1.0.6')
-//ther is proberbly a better way but i cant do it atm!!!//
+      info.addField('owners', "``"+config.devnames+"``")
+      info.addField('prefix', "``"+config.prefix+"``")
+      info.addField('verson', "``"+config.version+"``")
 
     message.channel.send(info)
     }}
