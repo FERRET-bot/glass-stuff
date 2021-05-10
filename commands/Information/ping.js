4200fd4c297aaee2dcdb20c32e270c8d05aedde7
Turtle-king <81062054+Turtle-king@users.noreply.github.com>
2021-05-10 10:42:17 +0100

    changed wording

--- commands/Information/ping.js
+++ commands/Information/ping.js
@@ -6,7 +6,7 @@ module.exports = {
     const pingembed = new Discord.MessageEmbed()
         .setColor('#FFA500')
         .setTitle('PONG!')
-        .setDescription('you have tested the timeing of this ferret')
+        .setDescription('you have tested the timeing of this cute bot!')
         .setImage('https://cdn.onlinewebfonts.com/svg/img_531810.png');
 
         message.channel.send(pingembed)
