bbd29531fc86650638758cd5e80676915203de71
Turtle-king <toomertracy696@Gmail.com>
2021-05-13 22:04:01 +0100

    fixed clear bug

--- commands/MOD STUFF/clear.js
+++ commands/MOD STUFF/clear.js
@@ -8,12 +8,12 @@ const Discord = require('discord.js'); // used for message embeds, etc
       async execute(message, args, bot, config){
           
         if (!config.devs.includes(message.author.id.toString())){return message.reply("Only my owners can use this command")}
-        const value = args[0] + 1
+        const value = args[0]
 
 
 
         if (value >= 100){return message.channel.send(`Value is to large, value = ${value} `);}
-
+        message.delete()
         message.channel.bulkDelete(value).then(promise => {message.channel.send("done!")})
 
 
