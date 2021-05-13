3011478b0706c78b910c1379357d2651848e4727
Turtle-king <toomertracy696@Gmail.com>
2021-05-13 17:38:47 +0100

    updated clear command to add 1 more message to delete the users messae

--- commands/MOD STUFF/clear.js
+++ commands/MOD STUFF/clear.js
@@ -8,7 +8,7 @@ const Discord = require('discord.js'); // used for message embeds, etc
       async execute(message, args, bot, config){
           
         if (!config.devs.includes(message.author.id.toString())){return message.reply("Only my owners can use this command")}
-        const value = args[0]
+        const value = args[0] + 1
 
 
 
