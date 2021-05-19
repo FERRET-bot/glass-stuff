f65022bb27a2cfeb91b463b2fb9926b569b13f16
Turtle-king <toomertracy696@Gmail.com>
2021-05-19 17:15:34 +0100

    Add a new command (newrole)

---
+++ commands/MOD STUFF/new role.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js'); // used for message embeds, etc
+
+  module.exports = {
+      name: 'newrole',
+      description: "makes a new role",
+      usage: "<name> <reason>",
+      cooldown: 5,
+      aliases: ["newrole"],
+      async execute(message, args, bot, config){
+
+        if (message.member.hasPermission("MANAGE_ROLES")){
+
+            const arr = message.content.split('/');
+
+
+         // Create a new role with data and a reason
+message.guild.roles.create({
+    data: {
+      name: arr[1],
+      color: "RANDOM",
+    },
+    reason: arr[2],
+  })
+    .catch(console.error);
+
+    message.channel.send('a new role has been created :^]')
+
+          
+      }}
+ };
