c2436957e8d7dc18e90d458207227db36039d44d
notthatkuna <new9gm@gmail.com>
2021-05-21 18:28:18 -0400

    add ID compatability to role and user

--- commands/MOD STUFF/addrole.js
+++ commands/MOD STUFF/addrole.js
@@ -3,14 +3,25 @@ const Discord = require('discord.js'); // used for message embeds, etc
 module.exports = {
     name: 'addrole',
     description: "Add a role to a user",
-    usage: "<user> <role>",
+    usage: "<user {@MENTION / ID}> <role {@MENTION / ID}>",
     cooldown: 5,
     aliases: ["addr"],
     async execute(message, args, bot, config){
         if(args.length <= 1) return message.reply("Please use the correct command format!");
-        const usertoadd = message.mentions.members.first();
-        const role = message.mentions.roles.first();
-        if(!usertoadd || !role) return message.reply("Please use the correct command format!");
+        var usertoadd = message.mentions.members.first();
+        var role = message.mentions.roles.first();
+        if(!usertoadd){
+            usertoadd = message.guild.members.cache.get(args[0].toString())
+            if(!usertoadd){
+                return message.reply("Could not find a user via @mention or {ID}, are you using the correct ``cmd`` format?")
+            }
+        }
+        if(!role){
+            role = message.guild.roles.cache.get(args[1].toString())
+            if(!role){
+                return message.reply("Could not find a role via @mention or {ID}, are you using the correct ``cmd`` format?")
+            }
+        }
         usertoadd.roles.add(role).then(promise=>{
             message.reply("Added role "+role.name+" to "+usertoadd.user.username)
         })
