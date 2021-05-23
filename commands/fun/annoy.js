8bca6140c51645483443858e0e18b4f53b44fb53
notthatkuna <new9gm@gmail.com>
2021-05-22 20:32:18 -0400

    Create annoy command

---
+++ commands/fun/annoy.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js'); // used for message embeds, etc
+
+module.exports = {
+    name: 'annoy',
+    description: "Turn all characters into spoilers (may make your friends hate you)",
+    usage: "<text>",
+    cooldown: 7,
+    aliases: ["spoilers"],
+    async execute(message, args, bot, config){
+        if(!args || !args[0]) return message.reply("Please specify text to send!");
+        var txt = args.join(" ");
+        var split = txt.split("")
+        var endstring = "||"
+        split.forEach(current=>{
+            endstring = endstring+""+current.toString()+"||"
+        })
+        message.channel.send(`\`\`${endstring}\`\``)
+    }
+};
