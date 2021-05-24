76bd2dfb46a91307263fcdc86db328342830922b
Turtle-king <toomertracy696@Gmail.com>
2021-05-24 22:20:41 +0100

    fixed hope

--- commands/Information/info.js
+++ commands/Information/info.js
@@ -14,13 +14,12 @@ module.exports = {
       info.setTitle('the current info about the bot')
       info.setThumbnail("https://www.bing.com/images/blob?bcid=RBE-Tm.CxroC5NvTZVNmjsTFKyOl.....88")
       info.setColor('#28ff00')
-      info.setDescription('the info seen below is the newest info about the bot')
+      info.setDescription('the info seen below is the newest info about the bot \n update log, config.updatelog')
       info.addField('owners', "``"+config.devnames+"``")
       info.addField('prefixes', "``"+config.prefixes+"``")
       info.addField('version', "``"+config.version+"``")
       info.addField('contact (email)', "``jaelyn@jaedev.net``")
       info.addField('------------------------------------------', "")
-      info.addField('update log', config.updatelog)
-
+      
     message.channel.send(info)
     }}
