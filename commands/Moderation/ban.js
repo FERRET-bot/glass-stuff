b4e0eab409de0a518a0ed7e86fef069fbf379073
Turtle-king <toomertracy696@Gmail.com>
2021-05-25 22:49:55 +0100

    .

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -6,16 +6,19 @@ const Discord = require('discord.js'); // used for message embeds, etc
       usage: "<name>",
       aliases: [],
       async execute(message, args, bot, config){
-          
-        if (!config.devs.includes(message.author.id.toString())||message.member.hasPermission("BAN_MEMBERS")){return message.reply("Only my owners can use this command")}
+        var passthru = false;
+        if(config.devs.includes(message.author.id.toString())) passthru = true
+        if(message.member.hasPermission("BAN_MEMBERS")) passthru = true
+        
         const victim = message.mentions.users.first();
 
-
+        if(!passthru) return
 
         message.guild.member(victim).ban().then(promise =>{
           message.channel.send(`${victim} has been banned from the server`)
         }).catch(err=>{message.reply(err)})
-    
+
+         
     
     
     
