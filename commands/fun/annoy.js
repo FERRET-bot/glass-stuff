b405763b993c83903519029fd77daf140278d2a7
notthatkuna <new9gm@gmail.com>
2021-05-25 20:08:04 -0400

    hotfix

--- commands/fun/annoy.js
+++ commands/fun/annoy.js
@@ -14,8 +14,7 @@ module.exports = {
         split.forEach(current=>{
             endstring = endstring+"||"+current.toString()+"||"
         })
-        var buffer = undefined;
-        if(endstring.length >= 1018) return message.channel.send(new Discord.MessageAttachment(Buffer.from(str, 'utf-8'), Date.now().toString()))
+        if(endstring.length >= 1018) return message.channel.send(new Discord.MessageAttachment(Buffer.from(endstring, 'utf-8'), Date.now().toString()))
         message.channel.send(`\`\`${endstring}\`\``)
     }
 };
