54fa3d9627bd009ed10aad32d86bfa631ba2af95
Turtle-king <toomertracy696@Gmail.com>
2021-05-26 19:28:42 +0100

    added tea command

---
+++ commands/work in progress/command..js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js'); // used for message embeds, etc
+
+  module.exports = {
+      name: 'tea',
+      description: "how to make a cup of tea",
+      usage: "",
+      cooldown: 100,
+      aliases: [],
+      async execute(message, args, bot, config){
+        const teaembed = new Discord.Messageembed
+
+        var thingstoadd = ["milk", "suger", "cold water", "hot water", "lava water", "a tea bag", "ice"]
+        var one =  Math.floor(array1.length * Math.random())
+        var two =  Math.floor(array1.length * Math.random())
+        var three =  Math.floor(array1.length * Math.random())
+        var four =  Math.floor(array1.length * Math.random())
+       
+       
+       
+        teaembed.settitle(`how to make a cup of tea`)
+        teaembed.setdescription(`first add ${one} into that cup\n\n then add ${two} to the cup\n\n after stir for around 5 seconds\n\n when you have finnished stiring add ${three}, the content may look weird but trust the proces\n\n whemn you are done with all that finnaly add ${four}\n\n\n and there you go\n a nice cup of whatever that thing is \n\n :)`)
+        teaembed.setColor('RANDOM')
+        
+
+        message.channel.send(teaembed)
+
+
+
+
+          
+      }
+ };
