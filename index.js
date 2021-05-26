f2e8ea20e371ce2a9450c20149101612d34fdcbd
notthatkuna <new9gm@gmail.com>
2021-05-26 16:41:47 -0400

    d

--- index.js
+++ index.js
@@ -98,6 +98,17 @@ try {
 
 bot.on("error", (err) => { bot.recenterrors.set(Date.now().toString(),err.toString()) });
 
+bot.on('messageDelete', (msg) => {
+    var fchn = msg.guild.channels.cache.find(channel => channel.name.toLowerCase() === 'glass-logs');
+    if(fchn){
+        const dt = new Date();
+        const msg = new Discord.MessageEmbed()
+        .setColor([219, 29, 73])
+        .setTitle("Bot detected a new deleted message!")
+        .setDescription(msg.content)
+        fchn.send(msg);
+    }
+})
 bot.on("message", async (message) => { // client or bot
     config = require('./config.json')
     if (message.channel.type == "dm") return;
