cc9ae5b844626052052432ee95bbe78c7cbf397d
notthatkuna <new9gm@gmail.com>
2021-05-26 16:46:48 -0400

    d

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ try {
 
 bot.on("error", (err) => { bot.recenterrors.set(Date.now().toString(),err.toString()) });
 
-bot.once('ready', (err) => {
+bot.once('ready', (err) => function (){
     bot.on('messageDelete', (msg) => {
         var fchn = msg.guild.channels.cache.find(channel => channel.name.toLowerCase() === 'glass-logs');
         if(fchn){
