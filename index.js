af5f5fe108a99a5491b7292d9741f3476b6a450d
notthatkuna <new9gm@gmail.com>
2021-05-26 18:11:27 -0400

    hotfix

--- index.js
+++ index.js
@@ -110,8 +110,8 @@ bot.on('ready', (err) => {
             .setDescription(msgdeletemsg.channel.name.toString()+"\n\n"+msgdeletemsg.content.toString()+"\n\nSent by "+msgdeletemsg.author.toString())
             fchn.send(msg);
             msgdeletemsg.attachments.forEach(a=>{
-                fchn.send(`User ${msgdeletemsg.author} sent a file which was deleted in a message:\n**WARNING::: If the return attachment is a file, leave handling to jaelyn**`,{
-                    file: a
+                fchn.send(`User ${msgdeletemsg.author} sent a file which was deleted in a message:\n\*\*WARNING::: If the return attachment is a file, leave handling to jaelyn\*\*`).then(mssss=>{
+                    fchn.send(a)
                 })
             })
         }
