d9381b026e8733ae30e228d166d4df1e22a02ee8
notthatkuna <new9gm@gmail.com>
2021-05-26 18:01:51 -0400

    hotfix

--- index.js
+++ index.js
@@ -111,7 +111,9 @@ bot.on('ready', (err) => {
             fchn.send(msg);
             msgdeletemsg.attachments.forEach(a=>{
                 const attch = new Discord.MessageAttachment(a)
-                fchn.send(`User ${msgdeletemsg.author} sent a file which was deleted in a message:\n**WARNING::: If the return attachment is a file, leave handling to jaelyn**\n${attch}`)
+                fchn.send(`User ${msgdeletemsg.author} sent a file which was deleted in a message:\n**WARNING::: If the return attachment is a file, leave handling to jaelyn**`,{
+                    file: attch.url
+                })
             })
         }
     })
