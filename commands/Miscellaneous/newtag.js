b03edfcebd878b7990e3b05bc550013642d5e14c
Turtle-king <toomertracy696@Gmail.com>
2021-05-27 17:29:29 +0100

    added userinfo command

---
+++ commands/Miscellaneous/newtag.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js'); // used for message embeds, etc
+
+  module.exports = {
+      name: 'userinfo',
+      description: "gets the info of a user",
+      usage: "[user]",
+      cooldown: 5,
+      aliases: [],
+      async execute(message, args, bot, config){
+
+      const embed = new MessageEmbed()
+      .setTitle(`${member.displayName}'s Info`)
+      .setThumbnail(member.user.displayAvatarURL({
+          dynamic: true
+      }))
+      .addField('User', member, true)
+      .addField('Discriminator', `\`#${member.user.discriminator}\``, true)
+      .addField('ID', `\`${member.id}\``, true)
+      .addField('Status', statuses[member.presence.status], true)
+      .addField('Bot', `\`${member.user.bot}\``, true)
+      .addField('Color Role', member.roles.color || '`None`', true)
+      .addField('Highest Role', member.roles.highest, true)
+      .addField('Joined server on', `\`${moment(member.joinedAt).format('MMM DD YYYY')}\``, true)
+      .addField('Joined Discord on', `\`${moment(member.user.createdAt).format('MMM DD YYYY')}\``, true)
+      .setFooter(message.member.displayName, message.author.displayAvatarURL({
+          dynamic: true
+      }))
+      .setTimestamp()
+      .setColor(member.displayHexColor);
+  if (activities.length > 0) embed.setDescription(activities.join('\n'));
+  if (customStatus) embed.spliceFields(0, 0, {
+      name: 'Custom Status',
+      value: customStatus
+  });
+  if (userFlags.length > 0) embed.addField('Badges', userFlags.map(flag => flags[flag]).join('\n'));
+  message.channel.send(embed);
+}
+};
