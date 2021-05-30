5a1268b789a9ee7a62749dfcecfaa1928e426b0d
notthatkuna <new9gm@gmail.com>
2021-05-30 15:53:33 -0400

    d

--- commands/Miscellaneous/wolf.js
+++ commands/Miscellaneous/wolf.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js'); // used for message embeds, etc
-const WolframAlphaAPI = require('wolfram-alpha-api');
-const waApi = WolframAlphaAPI('PXKTUY-Q5HQGYWXYK');
+const wajs = require('wajs');
+var waAppId = "PXKTUY-Q5HQGYWXYK"
+var waClient = new wajs(waAppId)
 
 
 module.exports = {
@@ -11,6 +12,17 @@ module.exports = {
     aliases: ["define"],
     async execute(message, args, bot, config){
         if(!args || !args[0]) return message.reply("invalid form")
-        waApi.getFull(args.join(' ')).then(console.log).catch(console.error);
+        var queryString = args.join(" ")
+        var queryOptions = {
+            format: 'image,plaintext',
+            units: 'metric'
+        }
+        waClient.query(queryString, queryOptions)
+        .then(function(resp) {
+            console.log(resp)
+        })
+        .catch(function(err) {
+            message.reply("There was an error fetching your query:\n"+err.toString())
+        })
     }
 };
