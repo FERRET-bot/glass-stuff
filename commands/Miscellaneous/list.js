3254e61e21be5887331ce3cd04c238a65ad6c560
notthatkuna <new9gm@gmail.com>
2021-06-01 00:49:07 -0400

    online pls!!

--- commands/Miscellaneous/list.js
+++ commands/Miscellaneous/list.js
@@ -1,5 +1,6 @@
-const Discord = require('discord.js');function fm(lsasarray){var s = "";lsasarray.forEach(item=>{s=s+"• "+item.toString()});return s;}module.exports = {name: 'list',description: 'format your list',usage: "[list]",cooldown: 5,aliases: ['ls', 'formatlist'],
+const Discord = require('discord.js');function fm(lsasarray){var s = "";lsasarray.forEach(item=>{s=s+"• "+item.toString()+"\n"});return s;}module.exports = {name: 'list',description: 'format your list',usage: "[list]",cooldown: 5,aliases: ['ls', 'formatlist'],
 execute(message,args,client,config){
+    args = args.join(" ").split(",");
     if(!args || !args[0]) return message.channel.send(fm(['Bacon','Milk','Eggs']))
     message.channel.send(fm(args))
 }}
