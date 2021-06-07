92296fdce994e178073a2953bf76674ece5363d8
Glitch (feline-rapid-hummingbird) <new9gm@gmail.com>
2021-06-07 21:02:21 +0000

    d

--- index.js
+++ index.js
@@ -141,13 +141,6 @@ bot.on("message", async (message) => { // client or bot
     }
     if (!pref) return;
     var cmd = bot.commands.get(command.toLowerCase() || bot.commands.find(cmcd => cmcd.aliases && cmcd.aliases.includes(command.toLowerCase())));
-    if(!cmd){
-        bot.commands.each(ccmd=>{
-            ccmd.aliases.forEach(al=>{
-                if(al == command.toLowerCase()) return cmd = ccmd
-            })
-        })
-    }
     if (cmd) {
 
         if (!bot.cooldowns.has(cmd.name.toString())) {
