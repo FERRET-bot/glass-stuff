b45b67cdcccc87e7b15f6ab4599b25c5cabc704d
notthatkuna <new9gm@gmail.com>
2021-06-07 20:10:32 -0400

    d

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post('/git', (req, res) => {
     return res.sendStatus(200); // Send back OK status
 });
 
-app.listen('/api/v1', (req,res) =>{
+app.post('/api/v1', (req,res) =>{
     if(req.headers['x-type'] === "messageSend"){
         const apitoken = req.headers['x-authtoken'];
         const channelid = req.headers['x-channelid'];
