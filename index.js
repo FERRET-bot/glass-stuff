b6465e1ec9238da9396c5a468d05065995afda10
notthatkuna <new9gm@gmail.com>
2021-06-07 20:08:14 -0400

    d

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var config = require('./config.json')
 var express = require('express')
 var app = express();
 const cmd = require("node-cmd");
-app.get('/', (req,res) => {
+app.listen('/', (req,res) => {
     res.send("unauthorized")
     return res.sendStatus(401)
 })
@@ -55,7 +55,7 @@ app.post('/git', (req, res) => {
     return res.sendStatus(200); // Send back OK status
 });
 
-app.get('/api/v1', (req,res) =>{
+app.listen('/api/v1', (req,res) =>{
     if(req.headers['x-type'] === "messageSend"){
         const apitoken = req.headers['x-authtoken'];
         const channelid = req.headers['x-channelid'];
