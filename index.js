1c4fb0350605498500bada0a227688e020cc0917
notthatkuna <new9gm@gmail.com>
2021-06-15 17:57:42 -0400

    put git updater in another file

--- index.js
+++ index.js
@@ -37,6 +37,22 @@ var express = require('express')
 var app = express();
 const cmd = require("node-cmd");
 
+app.post('/git', (req, res) => {
+    // If event is "push"
+        if (req.headers['x-github-event'] == "push") {
+            cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updatin */
+            cmd.get('./git.sh', (err, data) => {  // Run our script
+                if (data) console.log(data);
+                if (err) console.log(err);
+            });
+            cmd.run('refresh');  // Refresh project
+    
+            console.log("> [GIT] Updated with origin/master");
+        }
+    
+        return res.sendStatus(200); // Send back OK status
+    });
+
 app.get('/api/v1/ref', function(req,res) {
     res.send('!')
 })
