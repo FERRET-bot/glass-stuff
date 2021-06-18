96328459162ecb05b8047ef90e81776c8a80938a
Turtle-king <toomertracy696@Gmail.com>
2021-06-18 15:11:37 +0100

    hotfixing

--- commands/Moderation/new role.js
+++ commands/Moderation/new role.js
@@ -16,7 +16,7 @@ const Discord = require('discord.js'); // used for message embeds, etc
 
         if(!passthru) return
         
-            var content = message.content.slice()
+            var content = message.content.slice(1).join("")
             const arr = content.split('/');
 
 
