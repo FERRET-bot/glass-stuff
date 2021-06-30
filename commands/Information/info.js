f48928411fa1b72abebc946c637afd87d7e8b2d2
Turtle-king <toomertracy696@Gmail.com>
2021-06-30 13:45:50 +0100

    dissabled the info part

--- commands/Information/info.js
+++ commands/Information/info.js
@@ -11,12 +11,8 @@ module.exports = {
 
       message.channel.send("My prefixes are \""+config.prefixes+"\"")
 
-    let help = new client.disbut.MessageButton()   
 
-    help.setStyle('url')
-    help.setURL('https://discord.gg/U8ser55KqD') 
-    help.setLabel('join the support server!')
-    
+
       let info = new Discord.MessageEmbed
       info.setTitle('the current info about the bot')
       info.setThumbnail("https://www.bing.com/images/blob?bcid=RBE-Tm.CxroC5NvTZVNmjsTFKyOl.....88")
@@ -27,8 +23,12 @@ module.exports = {
       info.addField('version', "``"+config.version+"``")
       info.addField('contact (email)', "``jaelyn@jaedev.net``")
       
+    let help = new client.disbut.MessageButton()   
 
+    help.setStyle('url')
+    help.setURL('https://discord.gg/U8ser55KqD') 
+    help.setLabel('join the support server!')
     
-    message.channel.send(help, info)
+    message.channel.send(help)
 
     }}
