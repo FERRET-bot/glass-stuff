e1fb6872a64f14901e05c0c5ca9036591a999024
Turtle-king <toomertracy696@Gmail.com>
2021-06-30 14:04:09 +0100

    the invite is sent no in the embed

--- commands/Information/info.js
+++ commands/Information/info.js
@@ -13,17 +13,17 @@ module.exports = {
       message.channel.send("My prefixes are \""+config.prefixes+"\"")
 
       let info = new Discord.MessageEmbed
-      info.setTitle('the current info about the bot \nᶜˡᶦᶜᵏ ᵗᵒ ʲᵒᶦⁿ ᵗʰᵉ ˢᵘᵖᵖᵒʳᵗ ˢᵉʳᵛᵉʳ!')
+      info.setTitle('the current info about the bot')
       info.setThumbnail("https://www.bing.com/images/blob?bcid=RBE-Tm.CxroC5NvTZVNmjsTFKyOl.....88")
-      info.setURL("https://discord.gg/U8ser55KqD")
       info.setColor('#28ff00')
       info.setDescription('the info seen below is the newest info about the bot \n\n update log,'+config.updatelog)
       info.addField('owners', "``"+config.devnames+"``")
       info.addField('prefixes', "``"+config.prefixes+"``")
       info.addField('version', "``"+config.version+"``")
       info.addField('contact (email)', "``jaelyn@jaedev.net``")
+      info.setfooter("join the support server!")
       
 
-    message.channel.send(info)
+    message.channel.send(info, "https://discord.gg/U8ser55KqD")
 
     }}
